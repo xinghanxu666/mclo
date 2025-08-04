5cb1b4d276ba3a661de6b0acff2d76d5f880334b
星涵煦 <110706797+xinghanxu666@users.noreply.github.com>
2025-08-04 20:52:28 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 // 导入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
